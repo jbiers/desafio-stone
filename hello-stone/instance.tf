863b0de8bc135947d05bc7e250d4584aebc9db4e
julia <juliabiersuriano@gmail.com>
2022-09-26 15:14:56 -0400

    added ansible docker file

--- hello-stone/instance.tf
+++ hello-stone/instance.tf
@@ -12,6 +12,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "hello-stone" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  associate_public_ip_address = true
   key_name = "key"
 
   tags = {
@@ -20,6 +21,24 @@ resource "aws_instance" "hello-stone" {
 
   vpc_security_group_ids = [
 		"${aws_security_group.allow_ssh.id}",
-		"${aws_security_group.allow_http.id}"
+		"${aws_security_group.allow_http.id}",
+        "${aws_security_group.allow_tls.id}"
 	]
+
+    provisioner "remote-exec" {
+      inline = [
+        "echo Waiting for instance to be available"
+      ]
+
+      connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = file("~/key.pem")
+        host = aws_instance.hello-stone.public_ip
+      }
+    }
+
+    provisioner "local-exec" {
+      command = "ansible-playbook -i ${aws_instance.hello-stone.public_ip}, --private-key ${"~/key.pem"} docker.yaml"
+    }
 }
--- hello-stone/security-groups.tf
+++ hello-stone/security-groups.tf
@@ -16,9 +16,35 @@ resource "aws_security_group" "allow_ssh" {
 	}
 }
 
+resource "aws_security_group" "allow_tls" {
+	name        = "allow_tls"
+	description = "Allow TLS traffic"
+
+	ingress {
+		description      = "Allow TLS"
+		from_port        = 443
+		to_port          = 443
+		protocol         = "tcp"
+		cidr_blocks      = ["0.0.0.0/0"]
+		ipv6_cidr_blocks = ["::/0"]
+	}
+
+	egress {
+		from_port        = 443
+		to_port          = 443
+		protocol         = "tcp"
+		cidr_blocks      = ["0.0.0.0/0"]
+		ipv6_cidr_blocks = ["::/0"]
+	}
+
+	tags = {
+		Name = "allow_tls"
+	}
+}
+
 resource "aws_security_group" "allow_http" {
 	name        = "allow_http"
-	description = "Allow HTTP inbound traffic"
+	description = "Allow HTTP traffic"
 
 	ingress {
 		description      = "Allow HTTP"
